6d1828b4a366096a9868206d354e75d2eb2ece53
Ahbong Chang <cw.ahbong@gmail.com>
2016-04-10 10:37:31 +0800

    Prepare unit test environment

---
+++ cmake/Catch.cmake
@@ -0,0 +1,28 @@
+FUNCTION(PREPARE_CATCH)
+  IF(TARGET Catch)
+    RETURN()
+  ENDIF(TARGET Catch)
+  INCLUDE(ExternalProject)
+  ExternalProject_Add(
+    Catch
+    GIT_REPOSITORY https://github.com/philsquared/Catch.git
+    GIT_TAG c984fc3ecde60b59efa2203e82261acac8ac8502
+    TIMEOUT 10
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+  )
+  MESSAGE("Catch added...")
+ENDFUNCTION(PREPARE_CATCH)
+
+FUNCTION(ADD_CATCH_TEST_EXECUTABLE target_name)
+  PREPARE_CATCH()
+  ADD_EXECUTABLE(${target_name} ${ARGN})
+  ADD_TEST(NAME ${target_name} COMMAND ${target_name})
+
+  ExternalProject_Get_Property(Catch source_dir)
+  TARGET_INCLUDE_DIRECTORIES(${target_name} PRIVATE ${source_dir}/include)
+ENDFUNCTION(ADD_CATCH_TEST_EXECUTABLE)
